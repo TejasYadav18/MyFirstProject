226f4542a9cb2d606aa95fad13d8461629600973
TejasYadav18 <tejasyadav308@gmail.com>
2021-10-07 11:36:36 +0530

    second commit

--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -2,5 +2,6 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("hiii!!!Git");
         System.out.println("hello new git");
+        System.out.println("hiiiii newww gittt!!!");
     }
 }
